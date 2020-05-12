798ea2614ebffd40865cd1caa187954a254aa383
fabricio <fabricio.jacob@outlook.com>
2020-05-12 13:27:56 -0300

    add

--- inventario/src/main/java/br/com/spark/inventario/domain/model/enuns/Tipo.java
+++ inventario/src/main/java/br/com/spark/inventario/domain/model/enuns/Tipo.java
@@ -1,4 +1,23 @@
 package br.com.spark.inventario.domain.model.enuns;
 
-public class Tipo {
+import br.com.spark.inventario.domain.exceptions.TipoNaoEncontradoException;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+@Getter
+public enum Tipo {
+
+    ENTRADA("entrada"), SAIDA("saida");
+
+    private String descricao;
+
+    Tipo(final String descricao) {
+        this.descricao = descricao;
+    }
+
+    public static Tipo toEnum(final String descricao) {
+        return Stream.of(Tipo.values()).filter(t -> t.getDescricao().equals(descricao)).findFirst()
+                .orElseThrow(() -> new TipoNaoEncontradoException("Tipo não encontrado: " + descricao));
+    }
 }
--- inventario/src/main/java/br/com/spark/inventario/domain/service/facade/create/InventarioCreate.java
+++ inventario/src/main/java/br/com/spark/inventario/domain/service/facade/create/InventarioCreate.java
@@ -1,4 +1,46 @@
 package br.com.spark.inventario.domain.service.facade.create;
 
+import br.com.spark.inventario.domain.exceptions.InventarioNegativoException;
+import br.com.spark.inventario.domain.model.Inventario;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class InventarioCreate {
+
+    public Inventario criar(final List<Inventario> inventarios, final Inventario newEntity) {
+        Optional.of(inventarios)
+                .filter(v -> !v.isEmpty())
+                .ifPresentOrElse(invs -> {
+                    Long saldo = buscarSaldo(newEntity, invs);
+                    newEntity.setSaldo(saldo);
+                }, () -> {
+                    if(newEntity.getQuantidadeSaida() < 0 ){
+                        throw new InventarioNegativoException("Produto não encontra-se no inventario para efetuar saida. Codigo: " + newEntity.getCodigoProduto());
+                    }
+
+                    newEntity.setSaldo(newEntity.getQuantidadeEntrada());
+                });
+
+        return newEntity;
+    }
+
+    private Long buscarSaldo(Inventario inventario, List<Inventario> invs) {
+        return invs.stream()
+                .max(Comparator.comparing(Inventario::getId))
+                .map(inv -> calcularSaldo(inventario, inv)).get();
+    }
+
+    private Long calcularSaldo(Inventario inv, Inventario entity) {
+        var saldo = entity.getSaldo() + inv.getQuantidadeCorrente();
+
+        if (saldo < 0) {
+            throw new InventarioNegativoException("Inventario do produto encontra-se insuficiente. Codigo: " + entity.getCodigoProduto());
+        }
+
+        return saldo;
+    }
 }

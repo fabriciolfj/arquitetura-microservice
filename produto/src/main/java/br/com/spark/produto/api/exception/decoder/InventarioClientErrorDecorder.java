712ab94a210ed67115c8053e17057d6e49853e1d
fabricio <fabricio.jacob@outlook.com>
2020-05-13 08:25:53 -0300

    add

--- produto/src/main/java/br/com/spark/produto/api/exception/decoder/InventarioClientErrorDecorder.java
+++ produto/src/main/java/br/com/spark/produto/api/exception/decoder/InventarioClientErrorDecorder.java
@@ -1,4 +1,22 @@
 package br.com.spark.produto.api.exception.decoder;
 
-public class InventarioClientErrorDecorder {
+import br.com.spark.produto.api.exception.InventarioServiceException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+
+public class InventarioClientErrorDecorder implements ErrorDecoder {
+
+    private ErrorDecoder delegate = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        final var statusCode = HttpStatus.valueOf(response.status());
+
+        if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {
+            throw new InventarioServiceException("Falha ao consumir o servico");
+        }
+
+        return delegate.decode(methodKey, response);
+    }
 }

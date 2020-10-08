fbb3c01b95ef8ec71561fde8f537d5b588a1aed7
fabricio <fabricio.jacob@outlook.com>
2020-10-08 11:56:41 -0300

    add

--- produto/src/main/java/br/com/spark/produto/ProdutoApplication.java
+++ produto/src/main/java/br/com/spark/produto/ProdutoApplication.java
@@ -1,14 +1,17 @@
 package br.com.spark.produto;
 
+import br.com.spark.produto.domain.core.integration.client.InventarioClient;
 import br.com.spark.produto.domain.mensageria.saida.ProdutoSink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @EnableBinding(ProdutoSink.class)
 @EnableCircuitBreaker
 @SpringBootApplication
+@EnableFeignClients(basePackageClasses = InventarioClient.class)
 public class ProdutoApplication {
 
 	public static void main(String[] args) {
--- produto/src/main/java/br/com/spark/produto/domain/core/integration/config/FeignConfiguration.java
+++ produto/src/main/java/br/com/spark/produto/domain/core/integration/config/FeignConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableFeignClients(basePackageClasses = InventarioClient.class)
+//@Configuration
 public class FeignConfiguration {
 
     @Bean
